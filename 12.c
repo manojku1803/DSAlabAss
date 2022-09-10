2750acef6ccaf683c4ae5fd49b379dd552f8b4d2
Manoj <manojpradhan1803@gmail.com>
2022-09-10 23:11:05 +0530

    lo bhai 12wa badal diya

--- 12.c
+++ 12.c
@@ -4,26 +4,7 @@
 int main()
 {
     int n;
-printf("enter how many numbers ");
-    scanf("%d",&n);
-    int arr[n];
-    printf("enter the %d elemnts ",n);
-    for (int i = 0; i < n; i++) {
-        scanf("%d",&arr[i]);
-    }int x,y=0;
-    printf("which number you wanna search for ");
-    scanf("%d",&x);
-    for (int i = 0; i < n; i++) {
-    if(arr[i]==x){
-        y=i;
-        break;
-    }        
-    }if(y==0){
-        printf("%d not found ",x);
-    }else{
-        printf("%d is at %dth position ",x,y+1);
-    }
-    
+printf(" Bhaisaab iska code to delete ho gya ");
     return 0;
 }
 
